6427dd19e5358489aa22bc736b45059e71bd488f
Renato ST <renatostv@gmail.com>
2020-08-10 19:21:17 -0300

    Página de cadastro
    - Criação e estilização da página de cadastro
    
    - Funcção de adicionar novo campo de horarios na página de cadastro

---
+++ scripts/addField.js
@@ -0,0 +1,21 @@
+// Procurar o botão
+document.querySelector("#add-time")
+//Quando clicar no botão 
+.addEventListener('click', cloneField)
+
+
+//Executar uma ação
+function cloneField() {
+    //Duplicar os camposde horários
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    //Limpar os campos clonados
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    // Para cada campo, limpar
+    fields.forEach(field => field.value="") //arrow function
+
+    //Colocar na página, dentro da div schedule-items
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+    
+}
